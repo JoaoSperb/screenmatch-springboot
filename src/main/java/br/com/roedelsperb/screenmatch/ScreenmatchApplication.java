855d775fa52e9e056ff2995a86f10727608e3e0c
joao.sperb <j.roedel@edu.pucrs.br>
2024-04-30 12:57:26 -0300

    Criação do record DadosTemporada, e uso de um for na main, para percorrer todas as temporadas da serie

--- src/main/java/br/com/roedelsperb/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/roedelsperb/screenmatch/ScreenmatchApplication.java
@@ -2,11 +2,15 @@ package br.com.roedelsperb.screenmatch;
 
 import br.com.roedelsperb.screenmatch.model.DadosEpisodio;
 import br.com.roedelsperb.screenmatch.model.DadosSerie;
+import br.com.roedelsperb.screenmatch.model.DadosTemporada;
 import br.com.roedelsperb.screenmatch.service.ConsumoApi;
 import br.com.roedelsperb.screenmatch.service.ConversorDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -22,11 +26,21 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		System.out.println(json);
 
 		ConversorDados conversor = new ConversorDados();
-		DadosSerie serie = conversor.obterDados(json,DadosSerie.class);
-		System.out.println(serie);
+		DadosSerie dadosSerie = conversor.obterDados(json,DadosSerie.class);
+		System.out.println(dadosSerie);
 
 		json = consumoApi.obterDados("https://www.omdbapi.com/?t=lost&season=1&episode=3&apikey=3a7f6482");
 		DadosEpisodio dadosEpisodio = conversor.obterDados(json,DadosEpisodio.class);
 		System.out.println(dadosEpisodio);
+
+
+		List<DadosTemporada> temporadas = new ArrayList<>();
+
+		for(int i = 1; i<=dadosSerie.totalTemporadas();i++){
+			json = consumoApi.obterDados("https://www.omdbapi.com/?t=lost&season="+ i +"&apikey=3a7f6482");
+			DadosTemporada dadosTemporada = conversor.obterDados(json,DadosTemporada.class);
+			temporadas.add(dadosTemporada);
+		}
+		temporadas.forEach(System.out::println);
 	}
 }
--- src/main/java/br/com/roedelsperb/screenmatch/model/DadosEpisodio.java
+++ src/main/java/br/com/roedelsperb/screenmatch/model/DadosEpisodio.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosEpisodio(@JsonAlias("Title") String titulo,
                             @JsonAlias("Episode") Integer numEpisodio,
-                            @JsonAlias("Season") Integer temporada,
                             @JsonAlias("Released") String data,
                             @JsonAlias("imdbRating") String avaliacao) {
 }
