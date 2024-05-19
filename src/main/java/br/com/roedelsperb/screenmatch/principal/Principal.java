4881ca6ca2e73088b34e597e275e00ef580239f6
joao.sperb <j.roedel@edu.pucrs.br>
2024-05-19 15:43:19 -0300

    Implementa;'ao de fato da classe Principal, dando ao usuário a opção de digitar uma série, e através de números realizar determinada ação em relação a série, como dados do episódio, dados gerais da série e dados da temporada.

--- src/main/java/br/com/roedelsperb/screenmatch/principal/Principal.java
+++ src/main/java/br/com/roedelsperb/screenmatch/principal/Principal.java
@@ -5,37 +5,56 @@ import br.com.roedelsperb.screenmatch.model.DadosTemporada;
 import br.com.roedelsperb.screenmatch.service.ConsumoApi;
 import br.com.roedelsperb.screenmatch.service.ConversorDados;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
     private Scanner scanner = new Scanner(System.in);
-    private final String API_KEY = "&apikey=3a7f6482";
     private ConversorDados conversorDados = new ConversorDados();
+    private ConsumoApi consumoApi = new ConsumoApi();
+    private final String API_KEY = "&apikey=3a7f6482";
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+
 
     public void exibirMenu(){
 
         System.out.println("Digite o nome da série que você deseja saber as informações: ");
         String serie = scanner.nextLine();
-
-        var consumoApi = new ConsumoApi();
-        var json = consumoApi.obterDados("https://www.omdbapi.com/?t="+ serie + API_KEY);
-
+        serie = serie.replace(" ","+");
+        var json = consumoApi.obterDados(ENDERECO + serie + API_KEY);
 
         int leitura = -1;
 
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
         while (leitura!=0){
             System.out.println("Informe o que você deseja fazer, digitando o número correspondente.");
-            System.out.println("Digite 1 para dados do episódio.");
-            System.out.println("Digite 2 para dados gerais da serie.");
-            System.out.println("Digite 3 para dados gerais da temporada.");
+            System.out.println("Digite 1 para dados gerais da série.");
+            System.out.println("Digite 2 para dados gerais das temporadas.");
+            System.out.println("Digite 3 caso queira mudar a série em questão.");
             System.out.println("Digite 0 para sair.");
             leitura = scanner.nextInt();
 
-            if(leitura==2){
-                DadosSerie dadosSerie = conversorDados.obterDados(json,DadosSerie.class);
+            DadosSerie dadosSerie = conversorDados.obterDados(json,DadosSerie.class);
+
+            if(leitura==1){
                 System.out.println(dadosSerie);
-                leitura = scanner.nextInt();
             }
+            if(leitura==2){
+                for(int i = 1; i<=dadosSerie.totalTemporadas();i++){
+                    json = consumoApi.obterDados(ENDERECO + serie +"&season="+ i + API_KEY);
+                    DadosTemporada dadosTemporada = conversorDados.obterDados(json,DadosTemporada.class);
+                    temporadas.add(dadosTemporada);
+                }
+                temporadas.forEach(System.out::println);
+            }
+//            if(leitura==3){
+//                System.out.println("Informe a nova série.");
+//                serie = scanner.nextLine();
+//                serie = serie.replace(" ","+");
+//                json = consumoApi.obterDados(ENDERECO + serie + API_KEY);
+//            }
         }
     }
 }
